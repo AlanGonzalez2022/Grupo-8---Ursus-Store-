9a816840be6223bc9fad110bace49a7a59276cd9
Alan Gonzalez <alan.mtkm18@gmail.com>
2022-10-14 18:20:21 -0300

    modificando app.js para agregar nuestro template engine deseado

--- Ursus - FinalVersion/app.js
+++ Ursus - FinalVersion/app.js
@@ -4,6 +4,8 @@ const path=  require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs')
+
 const publicPath = path.resolve(__dirname, './public');
 app.use ( express.static(publicPath) );
 
